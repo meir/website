9af9ddac1a0e945b939b1e327e90a1c2ec8ca009
Michael Meir <michael.meir@notive.nl>
2023-02-01 13:24:47 +0100

    added automatic convertion to webp

--- app/main.go
+++ app/main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"flag"
 	"fmt"
-	"io"
 	"os"
 	"path"
 	"runtime"
 	"strings"
 	"time"
 
+	"github.com/flamingo-development/static/internal/assets"
 	"github.com/flamingo-development/static/internal/lexern2"
 )
 
@@ -73,7 +73,8 @@ func load_dir(f string, lexer *lexern2.Lexer) error {
 		}
 
 		if !strings.HasSuffix(file.Name(), ".htm") {
-			_, err := copy(path.Join(f, file.Name()), path.Join(*output, "assets", file.Name()))
+			err := assets.HandleFile(path.Join(f, file.Name()), *output)
+			// _, err := copy(path.Join(f, file.Name()), path.Join(*output, "assets", file.Name()))
 			if err != nil {
 				panic(err)
 			}
@@ -89,29 +90,4 @@ func load_dir(f string, lexer *lexern2.Lexer) error {
 	}
 
 	return nil
-}
-
-func copy(src, dst string) (int64, error) {
-	sourceFileStat, err := os.Stat(src)
-	if err != nil {
-		return 0, err
-	}
-
-	if !sourceFileStat.Mode().IsRegular() {
-		return 0, fmt.Errorf("%s is not a regular file", src)
-	}
-
-	source, err := os.Open(src)
-	if err != nil {
-		return 0, err
-	}
-	defer source.Close()
-
-	destination, err := os.Create(dst)
-	if err != nil {
-		return 0, err
-	}
-	defer destination.Close()
-	nBytes, err := io.Copy(destination, source)
-	return nBytes, err
 }
--- internal/assets/assets.go
+++ internal/assets/assets.go
@@ -1,13 +1,29 @@
 package assets
 
 import (
+	"bytes"
+	"image"
+	"image/gif"
+	"image/jpeg"
+	"image/png"
+	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 
+	"github.com/nickalie/go-webpbin"
 	"github.com/tdewolff/minify"
+	"github.com/tdewolff/minify/css"
+	"github.com/tdewolff/minify/js"
+	"golang.org/x/image/webp"
 )
 
 var min = minify.New()
+
+func init() {
+	min.AddFunc("text/css", css.Minify)
+	min.AddFunc("text/javascript", js.Minify)
+}
 
 func HandleFile(file string, buildfolder string) error {
 	// check file type
@@ -34,7 +50,7 @@ func HandleFile(file string, buildfolder string) error {
 			return err
 		}
 	case ".png", ".jpg", ".jpeg", ".gif":
-
+		return handleImage(file, buildfolder, content)
 	}
 
 	// write file to build folder/assets
@@ -49,4 +65,46 @@ func HandleFile(file string, buildfolder string) error {
 	}
 
 	return nil
+}
+
+func handleImage(file, buildfolder string, data []byte) error {
+	reader := bytes.NewReader(data)
+
+	var err error
+	var img image.Image
+
+	mimetype := http.DetectContentType(data)
+
+	switch mimetype {
+	case "image/png":
+		img, err = png.Decode(reader)
+	case "image/jpeg":
+		img, err = jpeg.Decode(reader)
+	case "image/gif":
+		img, err = gif.Decode(reader)
+	case "image/webp":
+		img, err = webp.Decode(reader)
+	default:
+		panic("unknown image type: " + mimetype)
+	}
+
+	if err != nil {
+		panic(err)
+	}
+
+	// convert to webp
+	// write to build folder/assets
+	filename := strings.TrimSuffix(filepath.Base(file), filepath.Ext(file))
+	f, err := os.Create(filepath.Join(buildfolder, "assets", filename+".webp"))
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+
+	err = webpbin.Encode(f, img)
+	if err != nil {
+		panic(err)
+	}
+
+	return err
 }
